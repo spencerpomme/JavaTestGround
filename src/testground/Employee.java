f95a58efe1841751e6be5e32a850d9662eb3a8f2
spencerpomme <spencerpomme@live.com>
2017-07-06 20:40:54 +0800

    add methods and fields to the class

--- src/testground/Employee.java
+++ src/testground/Employee.java
@@ -1,20 +1,51 @@
 package testground;
 
+import java.time.*;
+import java.util.Objects;
+
 public class Employee {
     private String name;
-    private int age;
+    private double salary;
+    private LocalDate hireDay;
     
-    public Employee(String name, int age) {
+    public Employee(String name, double salary, int year, int month, int day) {
         this.name = name;
-        this.age = age;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
     }
     
     public String getName() {
         return this.name;
     }
     
-    public int getAge() {
-        return this.age;
+    public double getSalary() {
+        return this.salary;
+    }
+    
+    public LocalDate getHireDay() {
+        return this.hireDay;
+    }
+    
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+    
+    public boolean equals(Object otherObject) {
+        // a quick test to see if the objects are identical
+        if (this == otherObject) return true;
+        // must return false if the explicit parameter is null
+        if (otherObject == null) return false;
+        //if the classes don't match, they can't be equal
+        if (this.getClass() != otherObject.getClass()) return false;
+        // now we know that otherObject is a non-null Employee
+        Employee other = (Employee) otherObject;
+        // test whether the fields have identical values
+        return Objects.equals(this.name, other.name) && this.salary == other.salary && Objects.equals(this.hireDay, other.hireDay);
+    }
+    
+    public int hashCode() {
+        return Objects.hash(this.name, this.salary, this.hireDay);
     }
 
 }
