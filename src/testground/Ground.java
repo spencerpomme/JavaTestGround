b2bfe535dd9a246692919ace71b4dad915ab40d4
spencerpomme <spencerpomme@live.com>
2017-07-06 21:12:51 +0800

    test wildcard

--- src/testground/Ground.java
+++ src/testground/Ground.java
@@ -45,5 +45,19 @@ public class Ground {
 	        res += num;
 	    return res;
 	}
+	
+	// test method for wildcard generic type
+	public static void mimaxBonus(Manager[] a, Pair<? super Manager> result) {
+	    if (a.length == 0) return;
+	    Manager min = a[0];
+	    Manager max = a[0];
+	    for (int i = 1; i < a.length; i++) {
+	        if (min.getBonus() > a[i].getBonus()) min = a[i];
+	        if (max.getBonus() < a[i].getBonus()) max = a[i];
+	    }
+	    result.setFirst(min);
+	    result.setSecond(max);
+	    
+	}
 
 }
